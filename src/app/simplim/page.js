eb34c6f29cd4cafcf27678844cfdefcd70b8269f
hossein <hossein@hosseins-MacBook-Pro.local>
2025-04-03 13:12:11 +0330

    add logs

--- src/app/simplim/page.js
+++ src/app/simplim/page.js
@@ -3,6 +3,6 @@ import React from "react";
 import AppLayout from "../components/AppLayout";
 
 export default function Home() {
-  console.log("sipdf");
+  console.log("sipdasdadf");
   return <AppLayout />;
 }
